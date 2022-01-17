d80ae9401c87c4ecdfd6f896d75d80ebc1f28a4b
jacob <jacobhertzlermartin@gmail.com>
2022-01-17 20:57:17 +0000

    trying again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app5432.get("/", (req, res) => {
     res.writeHead(200, {"Content-type": "text/html"});
     res.end(`
     <html>
-    <body>Adding second response on 5432, changing it for ci/cd test 1:47</body>
+    <body>Adding second response on 5432,Let's try this again</body>
     </html>`)
 });
 
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     res.writeHead(200, {"Content-type": "text/html"});
     res.end(`
     <html>
-    <body>changing it for ci/cd test 1:47?</body>
+    <body>Let's try this again</body>
     </html>`)
 });
 
