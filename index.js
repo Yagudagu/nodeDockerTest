4b71c40f6544d5f993d75b64efad75353706ef35
jacob <jacobhertzlermartin@gmail.com>
2022-02-02 15:27:08 +0000

    added other email ports

--- index.js
+++ index.js
@@ -2,32 +2,62 @@ const express = require("express");
 
 const app = express();
 
-const app5432 = express();
+
+
+const app2525 = express();
+
+const app587 = express();
+
+const app465 = express();
 
 app.use(express.json());
 
-app5432.get("/", (req, res) => {
-    res.writeHead(200, {"Content-type": "text/html"});
+app2525.get("/", (req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
     res.end(`
     <html>
-    <body>Adding second response on 5432,Let's try this again</body>
+    <body>Adding second response on 2525,Let's try this again</body>
+    </html>`)
+});
+
+app587.get("/", (req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`
+    <html>
+    <body>Adding second response on 587,Let's try this again</body>
+    </html>`)
+});
+
+app465.get("/", (req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`
+    <html>
+    <body>Adding second response on 465,Let's try this again</body>
     </html>`)
 });
 
 app.get("/", (req, res) => {
-    res.writeHead(200, {"Content-type": "text/html"});
+    res.writeHead(200, { "Content-type": "text/html" });
     res.end(`
     <html>
     <body>Let's try this again</body>
     </html>`)
 });
 
-const port =  process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 })
 
-app5432.listen(25, () => {
-    console.log(`Second server listening on 25`);
+app2525.listen(2525, () => {
+    console.log(`Second server listening on 2525`);
+})
+
+app587.listen(587, () => {
+    console.log(`Second server listening on 587`);
+})
+
+app465.listen(465, () => {
+    console.log(`Second server listening on 465`);
 })
